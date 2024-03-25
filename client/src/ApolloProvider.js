5de7fa3f206c4300b4aceb2f3f0124816c372c80
sujalsahu620 <sahusujal620@gmail.com>
2024-03-25 14:27:24 +0530

    done

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'https://idea-clan-assignment-eight.vercel.app/'
 });
 
 const authLink = setContext(() => {
