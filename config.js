47edacf238af47a932a88a74954844f55d2302bf
sujalsahu620 <sahusujal620@gmail.com>
2024-03-25 12:25:02 +0530

    done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://sahusujal620:sujal-123@cluster0.3n7waay.mongodb.net/ideaclan?retryWrites=true&w=majority&appName=Cluster0',
+  SECRET_KEY: 'some very secret key'
+};
