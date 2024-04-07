df5ec0091b4e87c8f58f4c1429f3fd99810cffc4
Sujal Sahu <sahusujal620@gmail.com>
2024-04-07 20:25:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://sahusujal620:sujal-123@cluster0.3n7waay.mongodb.net/ideaclan?retryWrites=true&w=majority&appName=Cluster0',
+    'mongodb+srv://sahusujal620:Sujal123@cluster0.3n7waay.mongodb.net/ideaclan',
   SECRET_KEY: 'some very secret key'
 };
